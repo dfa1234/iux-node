1acb58a99ddb3c11dd8c89ce0e85260c7dc89a9e
dfa <dfaure@outlook.com>
2019-06-16 19:22:48 +0300

    first commit

--- tools/io.ts
+++
@@ -1,8 +0,0 @@
-import {from, Observable} from 'rxjs';
-import * as fs from 'fs-extra';
-
-export const readFile$ = (path: string) => from(fs.readFile(path, 'utf8'));
-
-export const writeFile$ = (content: string, path: string) => from(fs.writeFile(path, content, 'utf8'));
-
-export const readDir$ = (path: string): Observable<string[]> => from(fs.readdir(path, 'utf8')) as any;
